90a0198a608ff21a2157945afa8221bfbc036c91
rowletter <hanskimqwerty@gmail.com>
2025-07-03 20:38:28 +0900

    Tab 2: RecyclerView added

--- app/src/main/java/com/example/favoriterestaurant/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/favoriterestaurant/ui/dashboard/DashboardFragment.kt
@@ -7,7 +7,36 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.favoriterestaurant.R
 import com.example.favoriterestaurant.databinding.FragmentDashboardBinding
+
+data class MyItem(val title: String, val description: String)
+
+class MyAdapter(private val itemList: List<MyItem>) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titleView: TextView = itemView.findViewById(R.id.itemTitle)
+        val descView: TextView = itemView.findViewById(R.id.itemDesc)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_layout, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.titleView.text = item.title
+        holder.descView.text = item.description
+    }
+
+    override fun getItemCount() = itemList.size
+}
 
 class DashboardFragment : Fragment() {
 
@@ -22,16 +51,20 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val recyclerView: RecyclerView = binding.recyclerView
+
+        val items: MutableList<MyItem> = mutableListOf()
+
+        for(i in 0 until 33) {
+            items.add(MyItem(title = "Title$i", description = "Description$i"))
         }
+
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 4)
+        recyclerView.adapter = MyAdapter(items)
+
         return root
     }
 
