b0e62a90bb9993fad63ba00ccc7e832fefd91e1b
seojin <seojin030527@gmail.com>
2025-07-05 13:14:16 +0900

    지도 기능 추가

--- app/src/main/java/com/example/favoriterestaurant/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/favoriterestaurant/ui/notifications/NotificationsFragment.kt
@@ -1,7 +1,7 @@
 package com.example.favoriterestaurant.ui.notifications
 
-import android.util.Log
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,13 +13,14 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.example.favoriterestaurant.R
 
 class NotificationsFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentNotificationsBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var map: GoogleMap
+    private var googleMap: GoogleMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,26 +33,24 @@ class NotificationsFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val mapFragment = childFragmentManager.findFragmentById(com.example.favoriterestaurant.R.id.map) as? SupportMapFragment
-        if (mapFragment == null) {
-            Log.e("MapDebug", "mapFragment is NULL!!")
-        } else {
-            Log.d("MapDebug", "mapFragment is ready, calling getMapAsync")
-            mapFragment.getMapAsync(this)
-        }
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        map = googleMap
-
-        // 원하는 위치로 마커 추가
-        val dgist = LatLng(35.7000, 128.4667) // 예시: DGIST
-        map.addMarker(MarkerOptions().position(dgist).title("Marker at DGIST"))
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(dgist, 15f))
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+        val dgist = LatLng(35.7000, 128.4667)
+        try {
+            googleMap?.addMarker(MarkerOptions().position(dgist).title("Marker at DGIST"))
+            googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(dgist, 15f))
+        } catch (e: Exception) {
+            Log.e("MapError", "Error adding marker: ${e.message}")
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        googleMap = null
     }
 }
